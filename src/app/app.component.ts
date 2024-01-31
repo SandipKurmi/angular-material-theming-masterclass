af25b4d3e5b7adcdbbed0f2155a64747bd8312c7
kurmicshare <kurmi.cshare@gmail.com>
2024-02-01 01:25:40 +0530

    Add home, heavy, footer components and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-import { AsyncPipe } from '@angular/common';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatCardModule } from '@angular/material/card';
+import { BannerComponent } from './banner/banner.component';
+import { HeavyComponentComponent } from './heavy-component/heavy-component.component';
+import { FooterComponentComponent } from './footer-component/footer-component.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
 
 interface Food {
   value: string;
@@ -24,33 +17,15 @@ interface Food {
   imports: [
     CommonModule,
     RouterOutlet,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatAutocompleteModule,
-    AsyncPipe,
-    MatSelectModule,
-    MatCardModule,
+    BannerComponent,
+    HeavyComponentComponent,
+    FooterComponentComponent,
+    HomeComponentComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-material-theming-masterclass';
-  selectedValue: string = '';
-  selectedCar: string = '';
-
-  foods: Food[] = [
-    { value: 'steak-0', viewValue: 'Steak' },
-    { value: 'pizza-1', viewValue: 'Pizza' },
-    { value: 'tacos-2', viewValue: 'Tacos' },
-  ];
-
-  sortData(sort: any) {
-    console.log(sort);
-  }
+  shouldLoadHeavyComponent = false;
 }
